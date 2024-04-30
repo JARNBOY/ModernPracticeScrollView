f30c98fdc5421d3cd13f9f1972c55f568d7fe0c3
Papon <papon.s@lawyerplusgroup.com>
2024-04-30 12:54:10 +0700

    Initial Commit

---
+++ ModernPracticeScrollView/ModernPracticeScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernPracticeScrollViewApp.swift
+//  ModernPracticeScrollView
+//
+//  Created by Papon Supamongkonchai on 30/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ModernPracticeScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
